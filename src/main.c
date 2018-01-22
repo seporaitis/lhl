0073ba6770fe4cc3e5c5ac713c9145a0ce5f288e
Julius Seporaitis <julius@seporaitis.net>
2018-01-22 16:51:39 +0000

    Add ncurses and CDK checks.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,27 @@
 #include <config.h>
+#include <ncurses.h>
 #include <stdio.h>
 
 
-int
-main(void) {
-  puts("Hello World!");
-  puts("This is " PACKAGE_STRING ".");
+int main(int argc, char *argv[])
+{
+  initscr();
+  cbreak();
+
+  keypad(stdscr, TRUE);
+
+  refresh();
+
+  int mx, my;
+  getmaxyx(stdscr, mx, my);
+
+  WINDOW *win = newwin(mx, my, 0, 0);
+  box(win, 0, 0);
+  wrefresh(win);
+
+  getch();
+
+  endwin();
 
   return 0;
 }
