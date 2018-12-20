fccd9f0c7ed81253438b99f5b95a15fcc50b6194
Julius Seporaitis <julius@seporaitis.net>
2018-12-20 22:58:05 +0000

    logging form mockup

--- src/main.c
+++ src/main.c
@@ -5,30 +5,62 @@
 #include "config.h"
 
 
-char message[] = ("This is a pretty long message. It will be displayed "
-                  "in a newt textbox, and illustrates how to construct "
-                  "a textbox from arbitrary text which may not have "
-                  "very good line breaks.\n\n"
-                  "Notice how literal \\n characters are respected, and "
-                  "may be used to force line breaks and blank lines.");
+struct calllog_s {
+  newtComponent co;
+  unsigned int ctr;
+  char buf[1024];
+};
+
+
+void updateCallLog(newtComponent co, void *data) {
+  struct calllog_s *callog = (struct calllog_s *)data;
+  const char *fmt = "%s20M   CW     20-Dec-18 22:00   %04d  %s        %d  %d  177\n";
+
+  callog->ctr++;
+  sprintf(callog->buf, fmt, callog->buf, callog->ctr, "LY2GV", 599, 599);
+  newtTextboxSetText(callog->co, callog->buf);
+}
+
 
 int main(void)
 {
-  newtComponent form, text, button;
+  newtComponent text;
+  unsigned int cols, rows;
+  char callsignVal[256] = {0};
+  char rstSentVal[256] = {0};
+  char rstRcvdVal[256] = {0};
+
+  char buf[1024] = {0};
 
   newtInit();
   newtCls();
 
-  text = newtTextboxReflowed(1, 1, message, 30, 5, 5, 0);
-  button = newtButton(12, newtTextboxGetNumLines(text) + 2, "Ok");
+  newtGetScreenSize(&cols, &rows);
+  newtPushHelpLine(NULL);
 
-  newtOpenWindow(10, 5, 37,
-                 newtTextboxGetNumLines(text) + 7, "Textboxes");
+  newtComponent form1 = newtForm(NULL, NULL, 0);
+  struct calllog_s callog = {
+    .co = newtTextbox(2, 2, cols - 6, 10, NEWT_FLAG_SCROLL),
+    .ctr = 0,
+    .buf = {0}
+  };
 
-  form = newtForm(NULL, NULL, 0);
-  newtFormAddComponents(form, text, button, NULL);
+  newtTextboxSetColors(callog.co, NEWT_COLORSET_BORDER, NEWT_COLORSET_LABEL);
+  newtFormAddComponents(form1, callog.co, NULL);
+  newtDrawForm(form1);
 
-  newtRunForm(form);
+  newtComponent form = newtForm(NULL, NULL, 0);
+  newtComponent band = newtLabel(2, 14, "20M");
+  newtComponent mode = newtLabel(8, 14, "CW");
+  newtComponent timestamp = newtLabel(16, 14, "20-Dec-18 22:00");
+  newtComponent counter = newtLabel(34, 14, "0002");
+  newtComponent callsign = newtEntry(40, 14, "", 10, &callsignVal, 0);
+  newtComponent rstSent = newtEntry(52, 14, "599", 10, &rstSentVal, 0);
+  newtComponent rstRcvd = newtEntry(60, 14, "599", 10, &rstRcvdVal, 0);
+  newtFormAddComponents(form, band, mode, timestamp, counter, callsign, rstSent, rstRcvd, NULL);
+  newtComponentAddCallback(rstRcvd, updateCallLog, &callog);
+  newtFormRun(form, NULL);
+
   newtFormDestroy(form);
   newtFinished();
 
