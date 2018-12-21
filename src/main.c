1b538156f90e52cc66f11592fbe2ff0592ec6e06
Julius Seporaitis <seporaitis@users.noreply.github.com>
2018-12-21 16:32:50 +0000

    Switch back to ncurses (#2)
    Part of #1 
    
    After investigating Newt as an option for terminal UI, decided not to go with it:
    
    * it severely lacks documentation - the only documentation I could find was [this](http://delphi.pd.infn.it/~saccarol/tutorial.html) and it is very lacking. One other option was to look at the examples of php or python bindings and translate them back to C, which is a bit backwards.
    * Newt has some limitations around window handling - e.g. windows are modal and only one can be active at one time - which makes it great for a sequence of form windows (e.g. installer, configuration), but not so great for interfaces where you need multiple different forms tiled around the screen.
    
    Ncurses is much more low level than Newt and even if there is CDK that could provide pre-made widgets - it lacks documentation as well. Anyhow, Ncurses itself is established enough and has various documentation bits and tutorials and videos on youtube about its usage that in the end it should be enough to be able to squeeze what I want from it.* Switch back to ncurses.
    
    * Add editorconfig
    
    * add newline to gitignore

--- src/main.c
+++ src/main.c
@@ -1,69 +1,18 @@
-#include <newt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 #include <unistd.h>
 
 #include "config.h"
 
 
-struct calllog_s {
-  newtComponent co;
-  unsigned int ctr;
-  char buf[1024];
-};
-
-
-void updateCallLog(newtComponent co, void *data) {
-  struct calllog_s *callog = (struct calllog_s *)data;
-  const char *fmt = "%s20M   CW     20-Dec-18 22:00   %04d  %s        %d  %d  177\n";
-
-  callog->ctr++;
-  sprintf(callog->buf, fmt, callog->buf, callog->ctr, "LY2GV", 599, 599);
-  newtTextboxSetText(callog->co, callog->buf);
-}
-
-
 int main(void)
 {
-  newtComponent text;
-  unsigned int cols, rows;
-  char callsignVal[256] = {0};
-  char rstSentVal[256] = {0};
-  char rstRcvdVal[256] = {0};
-
-  char buf[1024] = {0};
-
-  newtInit();
-  newtCls();
-
-  newtGetScreenSize(&cols, &rows);
-  newtPushHelpLine(NULL);
-
-  newtComponent form1 = newtForm(NULL, NULL, 0);
-  struct calllog_s callog = {
-    .co = newtTextbox(2, 2, cols - 6, 10, NEWT_FLAG_SCROLL),
-    .ctr = 0,
-    .buf = {0}
-  };
-
-  newtTextboxSetColors(callog.co, NEWT_COLORSET_BORDER, NEWT_COLORSET_LABEL);
-  newtFormAddComponents(form1, callog.co, NULL);
-  newtDrawForm(form1);
-
-  newtComponent form = newtForm(NULL, NULL, 0);
-  newtComponent band = newtLabel(2, 14, "20M");
-  newtComponent mode = newtLabel(8, 14, "CW");
-  newtComponent timestamp = newtLabel(16, 14, "20-Dec-18 22:00");
-  newtComponent counter = newtLabel(34, 14, "0002");
-  newtComponent callsign = newtEntry(40, 14, "", 10, &callsignVal[0], 0);
-  newtComponent rstSent = newtEntry(52, 14, "599", 10, &rstSentVal[0], 0);
-  newtComponent rstRcvd = newtEntry(60, 14, "599", 10, &rstRcvdVal[0], 0);
-  newtFormAddComponents(form, band, mode, timestamp, counter, callsign, rstSent, rstRcvd, NULL);
-  newtComponentAddCallback(rstRcvd, updateCallLog, &callog);
-  newtFormRun(form, NULL);
-
-  newtFormDestroy(form);
-  newtFinished();
+  initscr();
+  printw("Hello world.\n");
+  refresh();
+  getch();
+  endwin();
 
   return 0;
 }
