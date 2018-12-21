bcf0c1439948e59626d96b5ffb04d394d754bae3
Julius Seporaitis <seporaitis@users.noreply.github.com>
2018-12-21 19:54:32 +0000

    Structure the code a little bit. (#6)
    This extracts out qso form initialization and deinitialization into separate functions. It is not final structure, but a cleaner version for interim.

--- src/main.c
+++ src/main.c
@@ -8,21 +8,83 @@
 #include "config.h"
 
 
-enum {
-  FIELD_TIMESTAMP,
-  FIELD_MODE,
-  FIELD_BAND,
-  FIELD_CALLSIGN,
-  FIELD_RSTSENT,
-  FIELD_RSTRCVD,
-  FIELD_MAX,
-};
+/**
+ * QSO FORM
+ *
+ * Handles the QSO input form.
+ */
+
+
+typedef enum {
+  QSOF_TIMESTAMP,
+  QSOF_MODE,
+  QSOF_BAND,
+  QSOF_CALLSIGN,
+  QSOF_RSTSENT,
+  QSOF_RSTRCVD,
+  QSOF_MAX,
+} qsoform_field_t;
+
+
+typedef struct {
+  FIELD *field[QSOF_MAX + 1];
+  FORM *form;
+} qsoform_t;
+
+
+qsoform_t *new_qsoform() {
+  qsoform_t *obj = (qsoform_t *)malloc(sizeof(qsoform_t));
+
+  obj->field[QSOF_TIMESTAMP] = new_field(1, 10, 10, 1, 0, 0);
+  set_field_back(obj->field[QSOF_TIMESTAMP], A_UNDERLINE);
+  field_opts_off(obj->field[QSOF_TIMESTAMP], O_AUTOSKIP);
+
+  obj->field[QSOF_MODE] = new_field(1, 10, 10, 12, 0, 0);
+  set_field_back(obj->field[QSOF_MODE], A_UNDERLINE);
+  field_opts_off(obj->field[QSOF_MODE], O_AUTOSKIP);
+
+  obj->field[QSOF_BAND] = new_field(1, 10, 10, 23, 0, 0);
+  set_field_back(obj->field[QSOF_BAND], A_UNDERLINE);
+  field_opts_off(obj->field[QSOF_BAND], O_AUTOSKIP);
+
+  obj->field[QSOF_CALLSIGN] = new_field(1, 10, 10, 34, 0, 0);
+  set_field_back(obj->field[QSOF_CALLSIGN], A_UNDERLINE);
+  field_opts_off(obj->field[QSOF_CALLSIGN], O_AUTOSKIP);
+
+  obj->field[QSOF_RSTSENT] = new_field(1, 4, 10, 45, 0, 0);
+  set_field_back(obj->field[QSOF_RSTSENT], A_UNDERLINE);
+  field_opts_off(obj->field[QSOF_RSTSENT], O_AUTOSKIP);
+
+  obj->field[QSOF_RSTRCVD] = new_field(1, 4, 10, 50, 0, 0);
+  set_field_back(obj->field[QSOF_RSTRCVD], A_UNDERLINE);
+  field_opts_off(obj->field[QSOF_RSTRCVD], O_AUTOSKIP);
+
+  obj->field[QSOF_MAX] = NULL;
+
+  obj->form = new_form(obj->field);
+
+  return obj;
+}
+
+
+void free_qsoform(qsoform_t *obj) {
+  free_form(obj->form);
+
+  free_field(obj->field[QSOF_TIMESTAMP]);
+  free_field(obj->field[QSOF_MODE]);
+  free_field(obj->field[QSOF_BAND]);
+  free_field(obj->field[QSOF_CALLSIGN]);
+  free_field(obj->field[QSOF_RSTSENT]);
+  free_field(obj->field[QSOF_RSTRCVD]);
+
+  free(obj);
+}
+
 
 
 int main(void)
 {
-  FIELD *field[FIELD_MAX + 1];
-  FORM *form;
+  qsoform_t *qsoform;
   int ch;
 
   /* Initialize ncurses. */
@@ -31,91 +93,56 @@ int main(void)
   noecho();
   keypad(stdscr, TRUE);
 
-  /* Initialize the fields. */
-  field[FIELD_TIMESTAMP] = new_field(1, 10, 10, 1, 0, 0);
-  field[FIELD_MODE] = new_field(1, 10, 10, 12, 0, 0);
-  field[FIELD_BAND] = new_field(1, 10, 10, 23, 0, 0);
-  field[FIELD_CALLSIGN] = new_field(1, 10, 10, 34, 0, 0);
-  field[FIELD_RSTSENT] = new_field(1, 4, 10, 45, 0, 0);
-  field[FIELD_RSTRCVD] = new_field(1, 4, 10, 50, 0, 0);
-  field[FIELD_MAX] = NULL;
-
-  /* Set field options. */
-  set_field_back(field[FIELD_TIMESTAMP], A_UNDERLINE);
-  field_opts_off(field[FIELD_TIMESTAMP], O_AUTOSKIP);
-
-  set_field_back(field[FIELD_MODE], A_UNDERLINE);
-  field_opts_off(field[FIELD_MODE], O_AUTOSKIP);
-
-  set_field_back(field[FIELD_BAND], A_UNDERLINE);
-  field_opts_off(field[FIELD_BAND], O_AUTOSKIP);
-
-  set_field_back(field[FIELD_CALLSIGN], A_UNDERLINE);
-  field_opts_off(field[FIELD_CALLSIGN], O_AUTOSKIP);
-
-  set_field_back(field[FIELD_RSTSENT], A_UNDERLINE);
-  field_opts_off(field[FIELD_RSTSENT], O_AUTOSKIP);
-
-  set_field_back(field[FIELD_RSTRCVD], A_UNDERLINE);
-  field_opts_off(field[FIELD_RSTRCVD], O_AUTOSKIP);
-
-  /* Create a form. */
-  form = new_form(field);
-  post_form(form);
+  qsoform = new_qsoform();
+  post_form(qsoform->form);
   refresh();
 
   while ((ch = getch()) != KEY_F(1)) {
     switch (ch) {
     case KEY_RIGHT:
     case '\t':
-      form_driver(form, REQ_NEXT_FIELD);
-      form_driver(form, REQ_END_LINE);
+      form_driver(qsoform->form, REQ_NEXT_FIELD);
+      form_driver(qsoform->form, REQ_END_LINE);
       break;
     case KEY_LEFT:
-      form_driver(form, REQ_PREV_FIELD);
-      form_driver(form, REQ_END_LINE);
+      form_driver(qsoform->form, REQ_PREV_FIELD);
+      form_driver(qsoform->form, REQ_END_LINE);
       break;
     case KEY_BACKSPACE:
-      form_driver(form, REQ_DEL_PREV);
-      form_driver(form, REQ_END_LINE);
+      form_driver(qsoform->form, REQ_DEL_PREV);
+      form_driver(qsoform->form, REQ_END_LINE);
       break;
     case 10:
-      form_driver(form, REQ_VALIDATION);
+      form_driver(qsoform->form, REQ_VALIDATION);
       mvprintw(0, 1, "Timestamp: '%s'\nMode: '%s'\nBand: '%s'\nCallsign: '%s'\nRSTs: '%s'\nRSTr: '%s'\n",
-               field_buffer(field[FIELD_TIMESTAMP], 0),
-               field_buffer(field[FIELD_MODE], 0),
-               field_buffer(field[FIELD_BAND], 0),
-               field_buffer(field[FIELD_CALLSIGN], 0),
-               field_buffer(field[FIELD_RSTSENT], 0),
-               field_buffer(field[FIELD_RSTRCVD], 0));
+               field_buffer(qsoform->field[QSOF_TIMESTAMP], 0),
+               field_buffer(qsoform->field[QSOF_MODE], 0),
+               field_buffer(qsoform->field[QSOF_BAND], 0),
+               field_buffer(qsoform->field[QSOF_CALLSIGN], 0),
+               field_buffer(qsoform->field[QSOF_RSTSENT], 0),
+               field_buffer(qsoform->field[QSOF_RSTRCVD], 0));
 
-      form_driver(form, REQ_LAST_FIELD);
-      form_driver(form, REQ_CLR_FIELD);
-      form_driver(form, REQ_PREV_FIELD);
-      form_driver(form, REQ_CLR_FIELD);
-      form_driver(form, REQ_PREV_FIELD);
-      form_driver(form, REQ_CLR_FIELD);
-      form_driver(form, REQ_PREV_FIELD);
-      form_driver(form, REQ_CLR_FIELD);
-      form_driver(form, REQ_PREV_FIELD);
-      form_driver(form, REQ_CLR_FIELD);
-      form_driver(form, REQ_PREV_FIELD);
-      form_driver(form, REQ_CLR_FIELD);
+      form_driver(qsoform->form, REQ_LAST_FIELD);
+      form_driver(qsoform->form, REQ_CLR_FIELD);
+      form_driver(qsoform->form, REQ_PREV_FIELD);
+      form_driver(qsoform->form, REQ_CLR_FIELD);
+      form_driver(qsoform->form, REQ_PREV_FIELD);
+      form_driver(qsoform->form, REQ_CLR_FIELD);
+      form_driver(qsoform->form, REQ_PREV_FIELD);
+      form_driver(qsoform->form, REQ_CLR_FIELD);
+      form_driver(qsoform->form, REQ_PREV_FIELD);
+      form_driver(qsoform->form, REQ_CLR_FIELD);
+      form_driver(qsoform->form, REQ_PREV_FIELD);
+      form_driver(qsoform->form, REQ_CLR_FIELD);
       break;
     default:
-      form_driver(form, ch);
+      form_driver(qsoform->form, ch);
       break;
     }
   }
 
-  unpost_form(form);
-  free_form(form);
-  free_field(field[FIELD_TIMESTAMP]);
-  free_field(field[FIELD_MODE]);
-  free_field(field[FIELD_BAND]);
-  free_field(field[FIELD_CALLSIGN]);
-  free_field(field[FIELD_RSTSENT]);
-  free_field(field[FIELD_RSTRCVD]);
+  unpost_form(qsoform->form);
+  free_qsoform(qsoform);
 
   endwin();
 
